682f044197b5e16add78daf9be8b74ebf008601a
Minhtality <37172196+Minhtality@users.noreply.github.com>
2020-03-15 15:05:09 -0700

    Storybook upgrade

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-function loadStories() {
-  const req = require.context('../src', true, /\.stories\.jsx$/);
-  req.keys().forEach(filename => req(filename));
-}
-
-// automatically import all files ending in *.stories.js
-configure(loadStories, module);
