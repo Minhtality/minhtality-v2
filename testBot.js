774ee269b3596b1c4c60febf72b11899aafbd565
Minh Tran <mtran1712@gmail.com>
2020-11-27 22:45:53 -0800

    Testing newegg bot, need to get around detection

--- testBot.js
+++ testBot.js
@@ -3,8 +3,12 @@ const puppeteer = require  ('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const url = 'https://www.google.com/';
-    await page.goto(url);
+    page.setViewport({width: 1980, height: 1080});
+    const url = 'https://www.newegg.com/western-digital-black-sn750-nvme-1tb/p/N82E16820250110?Item=N82E16820250110&quicklink=true';
+    await page.goto(url, {"waitUntil" : "networkidle2"});
+    await page.click('button.btn.btn-primary.btn-wide');
+    await page.waitForSelector('.modal-content');
+    // await page.waitForSelector('button.a-button-close');
     await page.screenshot({path: 'testScreenshot.png'});
 
     await browser.close();
