ed818a64e06f23a7a4666d1405af02005ee59c83
minhtality <mtran1712@gmail.com>
2021-05-05 15:17:01 -0700

    Added mock quick copy to page. Pending format and typography

--- client/src/components/Resume/index.jsx
+++ client/src/components/Resume/index.jsx
@@ -5,10 +5,21 @@ const Resume = () => {
     return (
         <Styled.ResumeContainer>
             <Styled.SideBar >
-                <div>
-                    <Styled.Skills>Skills</Styled.Skills>
-                    <Styled.Languages>Language</Styled.Languages>
-                </div>
+                <Styled.SideBarContainer>
+                    <Styled.Skills>
+                        <h2>TECHNICAL SKILLS</h2>
+                        <ul>
+                            <li>Languages: Javascript, React,</li>
+                            <li></li>
+                        </ul>
+                    </Styled.Skills>
+                    <div>
+                        <h2>EDUCATION HEADING</h2>
+                        <p><span>2016</span>-<span>2018</span></p>
+                        <h3>California State University of Fullerton</h3>
+                        <p>Bachelor's degree. Information Science/Studies</p>
+                    </div>
+                </Styled.SideBarContainer>
             </Styled.SideBar>
             <Styled.Main>
                 <Styled.Header>
@@ -16,10 +27,10 @@ const Resume = () => {
                         src="https://media-exp1.licdn.com/dms/image/C4E03AQH3qBCPRsGTyA/profile-displayphoto-shrink_800_800/0/1575581866165?e=1625097600&v=beta&t=IUoQttLKMRn9ApMv3_mCW3ho0DELkRnV9m6Lkk9TINg" 
                         alt="Avatar"
                     />
-                    <div>Minh Tran</div>
+                    <h1>Minh Tran</h1>
                     <p>
                         <span>Front-End Developer</span>
-                        <span>Los Anegles</span>
+                        <span>Los Angeles</span>
                     </p>
                     <div>
                         <a href="#">email</a>
@@ -29,7 +40,7 @@ const Resume = () => {
                     </div>
                 </Styled.Header>
                 <Styled.AboutMe>
-                    <div>ABOUT ME</div>
+                    <h2>ABOUT ME</h2>
                     <p>
                     Ham tongue tri-tip, fatback chuck chicken t-bone porchetta burgdoggen kielbasa leberkas. Swine cupim drumstick frankfurter, biltong jowl jerky beef meatball porchetta fatback. Ground round beef landjaeger turducken, alcatra pancetta meatball salami capicola tail ball tip short loin kielbasa. Burgdoggen brisket cupim bresaola, jowl prosciutto short ribs pork chop capicola. Swine salami meatball, tail rump ground round meatloaf pastrami bresaola. Prosciutto sirloin cupim, strip steak pastrami drumstick jerky tongue landjaeger pork belly rump buffalo leberkas capicola.
                     </p>
@@ -38,9 +49,38 @@ const Resume = () => {
                     </p>
                 </Styled.AboutMe>
                 <Styled.ShowCase>
-                    <div>EXPERIENCE</div>
-                    <div>PROJECTS</div>
-                    <div>EDUCATION</div>
+                    <div>
+                        <h2>EXPERIENCE HEADING</h2>
+                        <p><span>12/2019</span>-<span>Present</span></p>
+                        <h3>Junior Front-end Developer</h3>
+                        <p><span>Team One USA | Lexus.com</span><span>Los Angeles</span></p>
+                        <ul>
+                            <li>Maintaining legacy application</li>
+                            <li>Handle multiple projects</li>
+                            <li>Implement component for model page redesign</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>PROJECTS HEADING</h2>
+                        <div>
+                            <p><span>06/2019</span>-<span>Present</span></p>
+                            <h3>Lexus.com | Adobe Experience Manager Migration</h3>
+                            <ul>
+                                <li>Thing one for AEM</li>
+                                <li>Thing two for AEM</li>
+                                <li>Thing three for AEM</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p><span>12/2019</span>-<span>06/2012</span></p>
+                            <h3>Lexus.com | Model Page Redesign</h3>
+                            <ul>
+                                <li>Thing one for model page redesign</li>
+                                <li>Thing two for model page redesign</li>
+                                <li>Thing three for model page redesign</li>
+                            </ul>
+                        </div>
+                    </div>
                 </Styled.ShowCase>
             </Styled.Main>
         </Styled.ResumeContainer>
--- client/src/components/Resume/index.styled.jsx
+++ client/src/components/Resume/index.styled.jsx
@@ -6,9 +6,25 @@ export const ResumeContainer = styled.div`
     width: 100%;
 `;
 export const SideBar = styled.div`
-    background: #264653;
+    background: #2C3546;
     width: 25%;
     color: #fff;
+    position: relative;
+`;
+export const SideBarContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 100%;
+    transform: translate(-50%, -50%);
+    margin: 0 auto;
+    display: block;
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
 `;
 export const Main = styled.div`
     width: calc(75% - 14%);
@@ -22,7 +38,9 @@ export const AboutMe = styled.section`
     margin-bottom: 40px;
 `;
 export const ShowCase = styled.section``;
-export const Skills = styled.div``;
+export const Skills = styled.div`
+
+`;
 export const Languages = styled.div``;
 export const Icon = styled.img`
     width: 10rem;
